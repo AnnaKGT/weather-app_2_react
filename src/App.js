5f1a2a9bbc98e7d3e7c8f0e79f2285e4dcf60fcf
AnnaKGT <apk2911@gmail.com>
2022-09-14 11:42:11 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -10,6 +10,33 @@ function App() {
       <header className="App-header">
         <WeatherSearch />
       </header>
+      <footer>
+        <span>
+          <a
+            href="https://github.com/AnnaKGT/weather-app_2_react"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Open-source code
+          </a>
+        </span>
+        <span> by </span>
+        <a
+          href="https://deluxe-starship-7c78b0.netlify.app/about.html"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Anna Kohut
+        </a>{" "}
+        <span> coding workshop from </span>
+        <a
+          href="https://www.shecodes.io/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          SheCodes
+        </a>
+      </footer>
     </div>
   );
 }
